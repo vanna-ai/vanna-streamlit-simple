684fdc9c73bc1e14957f9328c3172c9823b801aa
Zain Hoda <7146154+zainhoda@users.noreply.github.com>
2023-12-21 11:31:17 -0500

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simplified Vanna Streamlit App
+Chat with your database using Streamlit and Vanna.
+
+## Installation
+Virtual environment is recommended.
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Make sure to modify the Vanna setup in the `app.py` file.
+
+```bash
+streamlit run app.py
+```
---
+++ app.py
@@ -0,0 +1,36 @@
+# Import python packages
+import streamlit as st
+
+# ======= BEGIN VANNA SETUP =======
+
+import vanna as vn
+vn.set_api_key(YOUR_API_KEY)
+vn.set_model(YOUR_MODEL)
+vn.connect_to...(YOUR_DATABASE_CREDENTIALS)
+
+# ======= END VANNA SETUP =======
+
+my_question = st.session_state.get("my_question", default=None)
+
+if my_question is None:
+    my_question = st.text_input(
+        "Ask me a question about your data",
+    )
+else:
+    st.text(my_question)
+    
+    sql = vn.generate_sql(my_question)
+
+    st.text(sql)
+
+    df = vn.run_sql(sql)    
+        
+    st.dataframe(df, use_container_width=True)
+
+    plotly_fig = vn.generate_plotly(df)
+
+    code = vn.generate_plotly_code(question=my_question, sql=sql, df=df)
+
+    fig = vn.get_plotly_figure(plotly_code=code, df=df)
+
+    st.plotly_chart(fig, use_container_width=True)
